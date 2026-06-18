13a0a7327d03ca32ea6c05486305ad509822fba9
yuumuu <haidar.alhazen93@gmail.com>
2026-06-18 19:50:17 +0700

    fix bug case-sensitive

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../src/helpers/helper.php';
 require_once __DIR__ . '/../src/helpers/storage.php';
+require_once __DIR__ . '/../src/config/database.php';
 
 spl_autoload_register(function ($class) {
     $paths = [
